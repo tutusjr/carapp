082478bfb86d00afdc67b05651c3b8812103ff38
M.Mustafa Tutus <tutusfullstack@gmail.com>
2023-12-28 20:03:44 +0300

    date fns adjusted

--- src/app/components/DateSelection.js
+++ src/app/components/DateSelection.js
@@ -1,7 +1,55 @@
+"use client";
 
+import { useState } from "react";
+
+//handless ui
+import { Menu } from "@headlessui/react";
+
+//icons
+import { FaCalendarAlt } from "react-icons/fa";
+import { FaArrowRightLong } from "react-icons/fa6";
+
+//react date
+import { DateRange } from "react-date-range";
+import { format, addDays } from "date-fns";
+import { addYears } from 'date-fns';
+
+
+//react date range css
+import "react-date-range/dist/styles.css";
+import "react-date-range/dist/theme/default.css";
 
 export default function DateSelection() {
+  const [date, setDate] = useState([
+    {
+      startDate: new Date(),
+      endDate: null,
+      key: "selection",
+    },
+  ]);
+
   return (
-    <div>DateSelection</div>
-  )
+    <Menu as="div" className="w-full h-full flex xl:flex-row">
+      <div className="relative flex xl:block flex-col flex-1 xl:border-r xl:border-black/10 items-center justify-center">
+        {/* btn */}
+        <Menu.Button className="dropdown-btn w-content h-full flex flex-col justify-center items-center xl:items-start xl:pl-8">
+          <div className="flex flex-col xl:flex-row items-center xl:gap-x-2 gap-y-2 xl:gap-y-0">
+            <FaCalendarAlt className="text-accent" />
+            <div className="text-[15px] uppercase font-bold">Select Date</div>
+          </div>
+        </Menu.Button>
+        {/* menu */}
+        <Menu.Items className="drodown-menu shadow-lg absolute -top-96 xl:top-[90px] xl:left-0 z-50 transform -translate-x-1/2 xl:-translate-x-0 rounded-[10px] overflow-hidden">
+          <DateRange
+            editableDateInputs={true}
+            onChange={(item) => setDate([item.selection])}
+            moveRangeOnFirstSelection={false}
+            ranges={date}
+            rangeColors={["#ed1d24"]}
+            minDate={addDays(new Date(),0)}
+          />
+        </Menu.Items>
+      </div>
+    </Menu>
+  );
 }
--- src/app/components/LocationSelection.js
+++ src/app/components/LocationSelection.js
@@ -28,14 +28,25 @@ export default function LocationSelection() {
             <div className="flex flex-col justify-center ">
               <div className="flex flex-col xl:flex-row items-center xl:gap-x-2 gap-y-2 xl:gap-y-0">
                 <FaMapMarkerAlt className="text-accent" />
-                <div className="text-[15px] uppercase font-bold">Select Location</div> 
+                <div className="text-[15px] uppercase font-bold">
+                  Select Location
+                </div>
               </div>
-              <div className="uppercase font-medium text-[13px] text-secondary text-center xl:ml-6 xl:text-left">{location}</div>
+              <div className="uppercase font-medium text-[13px] text-secondary text-center xl:ml-6 xl:text-left">
+                {location}
+              </div>
             </div>
           </div>
         </Menu.Button>
         {/* items */}
-        <Menu.Items>Menu items</Menu.Items>
+        <Menu.Items className="drodown-menu shadow-lg absolute -top-56 xl:top-[90px] left-1/2 xl:left-0 z-10 transform -translate-x-1/2 xl:-translate-x-0 text-sm text-center xl:text-left w-full bg-white max-w-[332px] py-6 rounded-[10px]">
+          {locations.map((location,index) => (
+            <div
+            onClick={() => setLocation(location)} 
+            key={index} 
+            className="cursor-pointer py-4 xl:pl-10 hover:bg-gray-50 text-[13px] uppercase">{location}</div>
+          ))}
+        </Menu.Items>
       </div>
     </Menu>
   );
