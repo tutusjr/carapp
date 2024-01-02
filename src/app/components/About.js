6e7932a67082f2c6b4f80b6e9748b37ae6a7819a
M.Mustafa Tutus <tutusfullstack@gmail.com>
2024-01-02 19:47:17 +0300

    about whyus done

--- src/app/components/About.js
+++ src/app/components/About.js
@@ -1,7 +1,109 @@
+'use client'
+
+import Image from "next/image"
+
+import {MdOutLineMapsHomeWork, MdOutLineBuildCircle, MdOutlineDirectionsCar, MdOutlineMapsHomeWork, MdOutlineBuildCircle} from "react-icons/md"
+
+//react count up
+import CountUp from "react-countup"
+
+//react intersection observer
+import { useInView } from "react-intersection-observer"
+
+import {motion} from 'framer-motion'
+import {fadeIn} from '/variants'
 export default function About() {
+    const [ref, inView] = useInView({
+        threshold: 0.5,
+    })
     return (
-        <section className="section flex items-center bg-pink-200" id="about">
-            About
+        <section 
+        className="section flex items-center" 
+        id="about"
+        ref={ref}>
+            <div className="container mx-auto">
+                <div className="flex flex-col xl:flex-row xl:justify-between">
+                    {/* image */}
+                    <motion.div 
+                    variants={fadeIn("up", 0.2)}
+                    initial="hidden"
+                    whileInView={"show"}
+                    viewport={{ once: false, amount: 0.6 }}className="flex-1 mb-8 xl:mb-0">
+                        <Image 
+                        className="rounded-[20px]"
+                        src={"/images/about/car01.png"}
+                        width={600}
+                        height={448}
+                        alt="car"
+                        />
+                    </motion.div>
+                    {/* text / stats */}
+                    <div className="flex-1 flex xl:justify-center items-center">
+                        <div className="xl:max-w-[517px]">
+                            <motion.h2
+                            variants={fadeIn("up", 0.3)}
+                            initial="hidden"
+                            whileInView={"show"}
+                            viewport={{ once: false, amount: 0.6 }} 
+                            className="h2 mt-5 xl:mt-0">Car services simplified.</motion.h2>
+                            <motion.p 
+                            variants={fadeIn("up", 0.5)}
+                            initial="hidden"
+                            whileInView={"show"}
+                            viewport={{ once: false, amount: 0.6 }}
+                            className="mb-[42px] max-w-md">
+                                Rent, choose, and repair with ease. Our convenient locations, diverse car types, and reliable repair points ensure a seamless car experience.
+                            </motion.p>
+                            {/* stats */}
+                            <motion.div
+                            variants={fadeIn("up", 0.7)}
+                            initial="hidden"
+                            whileInView={"show"}
+                            viewport={{ once: false, amount: 0.6 }} 
+                            className="flex items-center gap-x-8 mb-12">
+                                {/* car types */}
+                                <div className="flex flex-col w-[100px]">
+                                    <MdOutlineDirectionsCar className="text-accent text-5xl xl:text-6xl"/>
+                                    <div className="text-3xl font-black">
+                                       {inView ? <CountUp start={0} end={50} duration={3}/> : null} +
+                                    </div>
+                                    <div className="uppercase text-[13px] font-semibold text-secondary">
+                                        car <br/> types
+                                    </div>
+                                </div>
+                                {/* rental outlets */}
+                                <div className="flex flex-col w-[100px]">
+                                    <MdOutlineMapsHomeWork className="text-accent text-5xl xl:text-6xl"/>
+                                    <div className="text-3xl font-black">
+                                       {inView ? <CountUp start={0} end={135} duration={3}/> : null}
+                                    </div>
+                                    <div className="uppercase text-[13px] font-semibold text-secondary">
+                                        rental <br/> outlets
+                                    </div>
+                                </div>
+                                {/* repair points */}
+                                <div className="flex flex-col w-[100px]">
+                                    <MdOutlineBuildCircle className="text-accent text-5xl xl:text-6xl"/>
+                                    <div className="text-3xl font-black">
+                                       {inView ? <CountUp start={0} end={35} duration={3}/> : null}
+                                    </div>
+                                    <div className="uppercase text-[13px] font-semibold text-secondary">
+                                        repair <br/> points
+                                    </div>
+                                </div>
+                                {/* btn */}
+        
+                            </motion.div>
+                            <motion.button
+                            variants={fadeIn("up", 0.9)}
+                            initial="hidden"
+                            whileInView={"show"}
+                            viewport={{ once: false, amount: 0.6 }} 
+                            className="hidden xl:block bg-accent hover:bg-accent-hover rounded-[10px] w-full h-16 uppercase font-meduim text-white tracking-[2px] text-[13px] max-w-[184px]">See all cars</motion.button>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </section>
     )
 }
--- src/app/components/Why.js
+++ src/app/components/Why.js
@@ -1,8 +1,101 @@
+"use client";
 
+import Image from "next/image";
+
+//framer motion
+import { motion } from "framer-motion";
+//variant
+import { fadeIn } from "/variants";
+//icons
+import { MdHandshake, MdKey, MdTrendingUp } from "react-icons/md";
 export default function Why() {
   return (
-    <section className="section flex items-center bg-green-200" id="why">
-      Why
+    <section className="section flex items-center" id="why">
+      <div className="container mx-auto">
+        <motion.h2
+          variants={fadeIn("up", 0.2)}
+          initial="hidden"
+          whileInView={"show"}
+          viewport={{ once: false, amount: 0.6 }}
+          className="h2 text-center"
+        >
+          Unmatched excellence and customer satisfaction
+        </motion.h2>
+        <motion.p
+          variants={fadeIn("up", 0.4)}
+          initial="hidden"
+          whileInView={"show"}
+          viewport={{ once: false, amount: 0.6 }}
+          className="max-w-[680px] text-center mx-auto mb-2"
+        >
+          Our deducation to providing exceptional services sets us apart from
+          the competition. From the moment you engage with us, we strive to
+          exceed your expectations in every interaction.
+        </motion.p>
+        {/* car image */}
+        <motion.div
+          variants={fadeIn("up", 0.6)}
+          initial="hidden"
+          whileInView={"show"}
+          viewport={{ once: false, amount: 0.6 }}
+          className="hidden md:flex justify-center mb-6 xl:mb-2"
+        >
+          <Image
+            src={"/images/why/car.svg"}
+            width={1060}
+            height={420}
+            alt="car"
+          />
+        </motion.div>
+        {/* grid items */}
+        <div className="flex flex-wrap justify-center xl:grid xl:grid-cols-3 gap-4 xl:gap-y-0 xl:gap-x-[30px]">
+          {/* item 1 */}
+          <motion.div
+            variants={fadeIn("right", 0.8)}
+            initial="hidden"
+            whileInView={"show"}
+            viewport={{ once: false, amount: 0.6 }}
+            className="flex flex-col items-center text-center max-w-[160px] xl:max-w-none p-2 xl:p-0"
+          >
+            <MdKey className="text-[38px] to-accent mb-4" />
+            <h3 className="h3">Rent simply and quickly</h3>
+            <p className="hidden xl:flex">
+              we prioritize your need and we go above and beyond to ensure your
+              experience with us is nothing short of outstanding.
+            </p>
+          </motion.div>
+          {/* item 2 */}
+          <motion.div
+            variants={fadeIn("up", 0.8)}
+            initial="hidden"
+            whileInView={"show"}
+            viewport={{ once: false, amount: 0.6 }}
+            className="flex flex-col items-center text-center max-w-[160px] xl:max-w-none p-2 xl:p-0"
+          >
+            <MdTrendingUp className="text-[38px] to-accent mb-4" />
+            <h3 className="h3">Modern $ well maintained vehicles</h3>
+            <p className="hidden xl:flex">
+              we prioritize your need and we go above and beyond to ensure your
+              experience with us is nothing short of outstanding.
+            </p>
+          </motion.div>
+          {/* item 3 */}
+          <motion.div
+            variants={fadeIn("left", 0.8)}
+            initial="hidden"
+            whileInView={"show"}
+            viewport={{ once: false, amount: 0.6 }}
+            className="flex flex-col items-center text-center max-w-[160px] xl:max-w-none p-2 xl:p-0"
+          >
+            <MdHandshake className="text-[38px] to-accent mb-4" />
+            <h3 className="h3">Propmt and flexible services</h3>
+            <p className="hidden xl:flex">
+              we prioritize your need and we go above and beyond to ensure your
+              experience with us is nothing short of outstanding.
+            </p>
+          </motion.div>
+        </div>
+      </div>
     </section>
-  )
+  );
 }
